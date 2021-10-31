976c13ef8e915aa5f8faedab7930aa9b4d048b12
Anatoli Stepanuke <anatolistepanuke@yandex.by>
2021-10-31 21:03:00 +0300

    Initial Commit

---
+++ eventChecker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  eventChecker
+//
+//  Created by Anatoli on 31.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
