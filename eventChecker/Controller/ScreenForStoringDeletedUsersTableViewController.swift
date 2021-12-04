b545ac0449c23635d89a9c3d97f40899309fad05
Anatoli Stepanuke <anatolistepanuke@yandex.by>
2021-12-04 19:41:10 +0300

    implemented cells using XIB PersonInfoViewCell

--- eventChecker/Controller/ScreenForStoringDeletedUsersTableViewController.swift
+++ eventChecker/Controller/ScreenForStoringDeletedUsersTableViewController.swift
@@ -4,6 +4,13 @@ final class ScreenForStoringDeletedUsersTableViewController: UITableViewControll
     //MARK: - Constants
     private let backgroundShadow = UIColor(red: 242/255, green: 242/255, blue: 242/255, alpha: 1)
     
+    //MARK: - Properties
+    private var deletedUsers: [String] = ["dsd", "dfdf", "fdfdfd"] {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
     // MARK: - Lyfecycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -14,7 +21,9 @@ final class ScreenForStoringDeletedUsersTableViewController: UITableViewControll
     
     // MARK: - Setups
     private func setupTableView() {
+        tableView.register(UINib(nibName: "PersonInfoTableViewCell", bundle: nil), forCellReuseIdentifier: "PersonInfoTableViewCell")
         tableView.backgroundColor = backgroundShadow
+        tableView.separatorStyle = .none
     }
     
     private func setupNavigationController() {
@@ -23,14 +32,14 @@ final class ScreenForStoringDeletedUsersTableViewController: UITableViewControll
     }
     
     // MARK: - Table view data source
-
-//    override func numberOfSections(in tableView: UITableView) -> Int {
-//        // #warning Incomplete implementation, return the number of sections
-//        return 0
-//    }
-//
-//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        // #warning Incomplete implementation, return the number of rows
-//        return 0
-//    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return deletedUsers.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PersonInfoTableViewCell", for: indexPath) as! PersonInfoTableViewCell
+//        cell.configure(using: deletedUsers[indexPath.row])
+        
+        return cell
+    }
 }
